6ed0ba80a4fe07e4d954a42b5ccf86da981db45a
LizaSmetan <krotovaliza@gmail.com>
2017-10-08 17:44:32 +0300

    почти получились селекты

--- app/js/init.js
+++ app/js/init.js
@@ -1,6 +1,6 @@
 $( function() {
 
-    // dateoicker init
+    // datepicker init
     var dateFormat = "mm/dd/yy",
         from = $( "#from" )
             .datepicker({
@@ -31,81 +31,71 @@ $( function() {
         return date;
     }
 
-
     // chart init
-    var ctx = document.getElementById("myChart");
-    if (ctx){
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ["01", "02", '03', '04', '05', '06', '07', '08', '09', '10'],
-                datasets: [
-                    {
-                        label: '# of Votes',
-                        data: [100, 190, 120, 200, 210, 150, 175, 143, 110, 130],
-                        backgroundColor: [
-                            'rgba(128, 191, 202, 1)',
-                            'rgba(255, 154, 56, 1)',
-                            'rgba(128, 191, 202, 1)',
-                            'rgba(255, 154, 56, 1)',
-                            'rgba(128, 191, 202, 1)',
-                            'rgba(255, 154, 56, 1)',
-                            'rgba(128, 191, 202, 1)',
-                            'rgba(255, 154, 56, 1)',
-                            'rgba(128, 191, 202, 1)',
-                            'rgba(255, 154, 56, 1)',
-                        ]
-                    }
-                ]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true
-                        }
-                    }]
-                }
-            }
-        });
-    }
-
+     // chart init
+     var ctx = document.getElementById("myChart");
+     var myChart = new Chart(ctx, {
+         type: 'bar',
+         data: {
+             labels: ["01", "02", "04", "05", "06", "07", "08", "09", "10"],
+             datasets: [{
+                 label: 'Signups',
+                 data: [100, 175, 135, 190, 100, 150, 175, 143, 110],
+                 backgroundColor: [
+                     'rgba(128, 191, 202, 1)',
+                     'rgba(128, 191, 202, 1)',
+                     'rgba(128, 191, 202, 1)',
+                     'rgba(128, 191, 202, 1)',
+                     'rgba(128, 191, 202, 1)',
+                     'rgba(128, 191, 202, 1)',
+                     'rgba(128, 191, 202, 1)',
+                     'rgba(128, 191, 202, 1)',
+                     'rgba(128, 191, 202, 1)',
+                 ],
+ 
+             },
+             {
+                 label: 'FTD',
+                 data: [190, 120, 200, 210, 150, 175, 143, 110, 130],
+                 backgroundColor: [
+                     'rgba(255, 154, 56, 1)',
+                     'rgba(255, 154, 56, 1)',
+                     'rgba(255, 154, 56, 1)',
+                     'rgba(255, 154, 56, 1)',
+                     
+                     'rgba(255, 154, 56, 1)',
+                     'rgba(255, 154, 56, 1)',
+                     'rgba(255, 154, 56, 1)',
+                     'rgba(255, 154, 56, 1)',
+                     'rgba(255, 154, 56, 1)',
+                 ],
+ 
+             }
+         ]
+         },
+         options: {
+             responsive: true,
+             scales: {
+                 yAxes: [{
+                     ticks: {
+                         beginAtZero:true
+                     }
+                 }]
+             }
+         }
+     });
+ 
 
     // modal init
-    var modalBtns = $('[data-modal]');
+    var modalBtn = $('.links .btn');
 
-    modalBtns.on('click', function (event) {
+    modalBtn.on('click', function (event) {
         event.preventDefault();
-
-        var target = $(this).attr('data-modal');
-
-        $(target).bPopup({
-            closeClass:'close-modal'
-        });
+        $('.modal').bPopup();
     });
 
-    // scrollbar init
-    $('.scrollbar-inner').scrollbar();
+    // formstyler
+    $('.filters-select').styler();
 
-
-    // range slider init
-    var handle = $( "#custom-handle" );
-    var range = $( "#slider" );
-    range.slider({
-        value: 60,
-        orientation: "horizontal",
-        range: "min",
-        animate: true,
-        max: 200,
-
-        create: function(event, ui) {
-            handle.append('<span class="value">'+$( this ).slider( "value" )+'hours</span>');
-            var value = handle.find('.value');
-            range.slider('option', 'valueSpan', value);
-        },
-        slide: function( event, ui ) {
-            range.slider( "option" ,"valueSpan").text(ui.value + 'hours');
-        }
-    });
 
 } );
